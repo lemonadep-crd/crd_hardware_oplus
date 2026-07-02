b6e4d83914b1424659a0a68ec3c70f9bfcf1da0a
abhinav2005git <abhinavanand.xda@gmail.com>
2026-07-02 09:13:13 +0000

    oplus-fwk: [TEST] fix NPE in oos-cam

--- oplus-fwk/src/oplus/content/res/OplusExtraConfiguration.java
+++ oplus-fwk/src/oplus/content/res/OplusExtraConfiguration.java
@@ -3,6 +3,4 @@ package oplus.content.res;
 public class OplusExtraConfiguration {
     
     public OplusExtraConfiguration() {}
-    public int mFont = 0;
-    public int mThemeChangedFlags = 0;
 }
